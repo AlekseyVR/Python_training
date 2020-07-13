52c5d4eb245ac865cd2fdaa24e9732d5a82f5159
Aleksey_VR <a.verzhins@gmail.com>
2020-07-14 00:31:12 +0300

    Добавлен тест на добавление контактов

---
+++ contact.py
@@ -0,0 +1,30 @@
+class Contact:
+    def __init__(self, first_name_contact, middle_name_contact, last_name_contact, nickname_contact,
+                 title_contact, company_contact, address_contact, home_contact, mobile_contact, work_contact,
+                 fax_contact, e_mail_contact, e_mail_2_contact, e_mail_3_contact, homepage_contact, birthday,
+                 birthdmonth,
+                 bYear, aDay, aMonth, aYear, secondary_address, secondary_home, notes_contact):
+        self.first_name_contact = first_name_contact
+        self.middle_name_contact = middle_name_contact
+        self.last_name_contact = last_name_contact
+        self.nickname_contact = nickname_contact
+        self.title_contact = title_contact
+        self.company_contact = company_contact
+        self.address_contact = address_contact
+        self.home_contact = home_contact
+        self.mobile_contact = mobile_contact
+        self.work_contact = work_contact
+        self.fax_contact = fax_contact
+        self.e_mail_contact = e_mail_contact
+        self.e_mail_2_contact = e_mail_2_contact
+        self.e_mail_3_contact = e_mail_3_contact
+        self.homepage_contact = homepage_contact
+        self.birthday = birthday
+        self.birthdmonth = birthdmonth
+        self.bYear = bYear
+        self.aDay = aDay
+        self.aMonth = aMonth
+        self.aYear = aYear
+        self.secondary_address = secondary_address
+        self.secondary_home = secondary_home
+        self.notes_contact = notes_contact
