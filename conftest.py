cc84402bde6243d38bfb028a9157307ed92d90d8
Aleksey Verzhinskiy-Ryng <a.verzhinskiy-ryng@melsoft-games.com>
2020-08-20 00:31:59 +0300

    Реализованы опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения

--- conftest.py
+++ conftest.py
@@ -7,12 +7,16 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseurl")
     if fixture is None:
-        fixture = Application()
+        browser = request.config.getoption("--browser")
+        base_url = request.config.getoption("--baseurl")
+        fixture = Application(browser=browser, base_url=base_url)
         fixture.session.login(username="admin", password="secret")
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
             fixture.session.login(username="admin", password="secret")
     return fixture
 
@@ -25,3 +29,8 @@ def stop(request):
 
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseurl", action="store", default="http://localhost/addressbook/")
--- test/Test_Add_Group.py
+++ test/Test_Add_Group.py
@@ -14,11 +14,11 @@ testdata = [Group(name_group="", logo_group="", footer_group="")] + [
     # Group(name_group="first_group1", logo_group="first_logo2", footer_group="first_foot3"),
     # Group(name_group="", logo_group="", footer_group=""),
     Group(name_group=random_string("name", 10), logo_group=random_string("logo", 20),
-          footer_group=random_string("footer", 20))
+          footer_group=random_string("footer", 10))
 
     # Group(name_group=name_group, logo_group=logo_name, footer_group=footer_group)
 
-    for i in range(5)
+    for i in range(2)
     # for name_group in ['', random_string('name', 10)]
     # for logo_name in ['', random_string('logo', 10)]
     # for footer_group in ['', random_string('footer', 10)]
