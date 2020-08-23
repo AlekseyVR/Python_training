f8cc37238db2b168b82311390e959f54b5b0247c
Aleksey Verzhinskiy-Ryng <a.verzhinskiy-ryng@melsoft-games.com>
2020-08-23 21:54:38 +0300

    Динамическая генерация тестов Test_add_group

---
+++ data/groups.py
@@ -0,0 +1,8 @@
+from models.group import Group
+
+
+# для исп константы в тесте изменить импорт на: from data.add_group import constant as testdata
+testdata = [
+    Group(name_group="Name1", logo_group="logo1", footer_group="footer1"),
+    Group(name_group="Name2", logo_group="logo2", footer_group="footer2")
+]
