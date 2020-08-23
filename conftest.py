d2dbd40ab33565d27659a42b4b500673acc3cc71
Aleksey Verzhinskiy-Ryng <a.verzhinskiy-ryng@melsoft-games.com>
2020-08-23 15:15:27 +0300

    Реализовна загрузка информации от тестируемом приложении из конфигурационного файла

--- conftest.py
+++ conftest.py
@@ -1,23 +1,27 @@
 from fixture.application import Application
 import pytest
+import json
+import os.path
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseurl")
-    if fixture is None:
-        browser = request.config.getoption("--browser")
-        base_url = request.config.getoption("--baseurl")
-        fixture = Application(browser=browser, base_url=base_url)
-        fixture.session.login(username="admin", password="secret")
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-            fixture.session.login(username="admin", password="secret")
+
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        # browser = request.config.getoption("--browser")
+        # base_url = request.config.getoption("--baseurl")
+        fixture = Application(browser=browser, base_url=target['baseurl'])
+        fixture.session.login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -33,4 +37,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseurl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
