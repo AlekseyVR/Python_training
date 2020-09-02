852f4b9d4e3e45d5491b99fb1804a478c8e0c247
Aleksey Verzhinskiy-Ryng <a.verzhinskiy-ryng@melsoft-games.com>
2020-09-02 19:57:58 +0300

    Переделываем тесты: проверяем данные, загруженные из базы данных

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,16 +1,20 @@
-from random import randrange
+import random
 from models.group import Group
 
 
-def test_delete_some_group(app):
+def test_delete_some_group(app, db):
     # если число чек-боксов группы равно нулю, то создаем группу
-    if app.group.count() == 0:
+    # if app.group.count() == 0:
+    if len(db.get_group_list()) == 0:
         app.group.create(Group(name_group="test_for_Del"))
     # удаляем группу
-    old_groups = app.group.get_group_list()
-    index = randrange(len(old_groups))
-    app.group.delete_group_by_index(index)
-    new_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
+    group = random.choice(old_groups)
+    # index = randrange(len(old_groups))
+    app.group.delete_group_by_id(group.id)
+    # app.group.delete_group_by_index(index) через бд не годится
+
+    new_groups = db.get_group_list()
     assert (len(old_groups) - 1 == len(new_groups))  # можно ее удалить, но останется для пустых спиков или др. проблем
-    old_groups[index:index+1] = []
+    old_groups.remove(group)
     assert old_groups == new_groups
